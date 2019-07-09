862d5d038fb4ba7f7544907420306a18e41f9fb0
Christian Schulte <cschulte@kth.se>
2019-07-09 14:27:16 +0200

    Updated

--- build/src/software.php
+++ build/src/software.php
@@ -36,7 +36,7 @@ href="https://www.ericsson.com/research-blog/outperforming-state-art-compilers-u
 
 <h2 id="gecode">Gecode</h2>
 
-<a href="http://www.gecode.org/"
+<a href="https://www.gecode.org/"
   ><img class="right"
         alt="Gecode Logo"
         src="images/gecode.png"
@@ -44,7 +44,7 @@ href="https://www.ericsson.com/research-blog/outperforming-state-art-compilers-u
 
 <p>
 I am the main designer and developer of <a
-href="http://www.gecode.org">Gecode</a>.  Gecode is an open,
+href="https://www.gecode.org">Gecode</a>.  Gecode is an open,
 free, portable, accessible, and efficient environment for
 developing constraint-based systems and applications in research,
 industry, and education.
@@ -54,11 +54,11 @@ Particularly important for its design is
 simplicity and accessibility. Simplicity is the key reason
 why Gecode is efficient and successfully exploits today's
 commodity parallel hardware. Accessibility is due to its 
-complete <a href="http://www.gecode.org/doc-latest/reference/"
+complete <a href="https://www.gecode.org/doc-latest/reference/"
 >reference documentation</a>, complete
-<a href="http://www.gecode.org/documentation.html">tutorial
+<a href="https://www.gecode.org/documentation.html">tutorial
 documentation</a>, and
-<a href="http://www.gecode.org/publications.html">academic publications</a> in conferences and journals presenting key
+<a href="https://www.gecode.org/publications.html">academic publications</a> in conferences and journals presenting key
 design decisions and techniques.
 </p>
 
@@ -71,7 +71,7 @@ companies; it is included in distributions such as Debian, Ubuntu, OpenSUSE, Gen
 
 <p class="box">
 For technical questions about Gecode, please do not ask me directly but use the 
-<a href="http://www.gecode.org/community.html">mailing list</a>: it
+<a href="https://www.gecode.org/community.html">mailing list</a>: it
   is better to build a comprehensive archive of questions and
   answers related to Gecode that can be useful for everyone.
   If you have any questions not suitable for the public mailing list,
@@ -98,7 +98,7 @@ platform for constraint and distributed programming, from 1997 to 2001.
 My contributions to Mozart in
 particular include the design and implementation of constraint
 services, which is documented in the book <a
-href="http://link.springer.de/link/service/series/0558/tocs/t2302.htm">Programming
+href="https://link.springer.com/book/10.1007/3-540-45945-6">Programming
 Constraint Services</a>.
 </p>
 
