90428386af902e14c02227d51421681d0a34c68c
Christian Schulte <cschulte@kth.se>
2019-08-22 10:21:22 +0200

    Local fonts

--- build/src/defaults.php
+++ build/src/defaults.php
@@ -76,7 +76,6 @@
 <head>
 	<meta http-equiv="content-type" content="text/html"; charset="utf-8" />
 	<meta name="author" content="Christian Schulte" />
-        <link href="https://fonts.googleapis.com/css?family=Cabin:400,400i,600,600i" rel="stylesheet">
 	<link rel="stylesheet" type="text/css" 
               href="<?php echo $urlroot; ?>style.css" title="1024px" media="screen,projection" />
 	<title><?php echo $title; ?></title>
